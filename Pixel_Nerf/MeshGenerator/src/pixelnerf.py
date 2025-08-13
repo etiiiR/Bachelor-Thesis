4285486f9cdbc96fb9d2b382f754b08b77e1a3e3
Etienne Roulet <etienne.roulet@outlook.com>
2025-08-13 15:19:28 +0200

    changed testing candiates to real ones again

--- Pixel_Nerf/MeshGenerator/src/pixelnerf.py
+++ Pixel_Nerf/MeshGenerator/src/pixelnerf.py
@@ -242,7 +242,7 @@ class EvalMetricsRunner:
         total_objs = len(data_loader)
 
         best_mesh_thresh = None
-        mesh_thresh_candidates = [0.1]
+        mesh_thresh_candidates = [0.1, 0.15, 0.2, 0.5, 1.0, 2.0, 5.0, 7.5, 10.0]
 
         # --- Mesh threshold search: only ONCE for the whole test set ---
         if args.gen_meshes and args.find_best_mesh_thresh:
